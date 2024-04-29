0a4527d273de9e92fea0afd44a4448c9b8523c9a
Sam Miller <61556383+sam-w-miller@users.noreply.github.com>
2024-04-29 15:56:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
 let distance2 = 0
-I2C_LCD1602.LcdInit(0)
+basic.showNumber(0)
 basic.forever(function () {
-    I2C_LCD1602.clear()
-    I2C_LCD1602.ShowString("Distance is:", 0, 0)
     distance2 = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P0,
     PingUnit.Centimeters
     )
-    I2C_LCD1602.ShowNumber(distance2, 12, 0)
+    basic.showNumber(distance2)
     basic.pause(1000)
 })
