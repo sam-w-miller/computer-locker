a601ce4efd76f7ad3d2d1969a23d7a138a6a52f9
Sam Miller <61556383+sam-w-miller@users.noreply.github.com>
2024-05-01 19:09:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 let distance2 = 0
-basic.showNumber(0)
+basic.showString("Hi")
+I2C_LCD1602.LcdInit(0)
 basic.forever(function () {
+    I2C_LCD1602.clear()
+    I2C_LCD1602.ShowString("Distance is:", 0, 0)
     distance2 = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P0,
     PingUnit.Centimeters
     )
-    basic.showNumber(distance2)
-    basic.pause(1000)
+    led.plotBarGraph(
+    distance2,
+    200
+    )
+    I2C_LCD1602.ShowNumber(distance2, 12, 0)
+    if (distance2 < 10) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+    basic.pause(200)
 })
